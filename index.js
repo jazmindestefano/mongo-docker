64997abb533f64ef68304a89fc948c9f48a99a50
jazmindestefano <jazminisabeldestefano@gmail.com>
2024-10-03 14:02:56 -0300

    feat: update index js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const app = express();
+import express from 'express'
+import mongoose from 'mongoose'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+  tipo: String,
+  estado: String,
+}))
+
+const app = express()
+
+mongoose.connect('mongodb://jazmin:admin12345@monguito:27017/?ssl=true&replicaSet=atlas-kdbq33-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
+
+app.get('/', async (_req, res) => {
+  console.log('listando... chanchitos...')
+  const animales = await Animal.find();
+  return res.send(animales)
+})
+app.get('/crear', async (_req, res) => {
+  console.log('creando...')
+  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
+  return res.send('ok')
+})
+
+app.listen(3000, () => console.log('listening...'))
